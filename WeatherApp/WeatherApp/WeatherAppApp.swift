ade275076497956abc2a4580957161f63e8e5910
Darya Viter <cleverdevil@yandex.ru>
2023-09-10 12:30:19 +0300

    create app & add linter

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Darya Viter on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
